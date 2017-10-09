e4139a3a6d87507fcf0f127a664322d70ea59b7f
Jinhyun, Nam <jlj4348@naver.com>
2017-10-10 01:41:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 var curY= function(){
 	if(typeof(window.pageYoffset) == 'number')
 		return window.pageYoffset;
@@ -17,16 +16,78 @@ var navi_lock = function()
 	{
 		if(curY() <= navi.offsetTop){
 			if(navi.style.position == 'fixed'){
-				logo.style.display = 'block';
+				banner.style.display = 'block';
 				navi.style.position = 'static';
-				scrollTo(0, 200);
+				scrollTo(0, navi.offsetTop);
 			}
 		}
 		else{
 			if(navi.style.position != 'fixed'){
-				logo.style.display = 'none';
+				banner.style.display = 'none';
 				navi.style.position = 'fixed';
 			}
 		}
 	};
+};
+
+function scrollToItem(item) {
+    var diff=(item.offsetTop - window.scrollY)/8
+    if (Math.abs(diff)>1) {
+        window.scrollTo(0, (window.scrollY+diff))
+        clearTimeout(window._TO)
+        window._TO=setTimeout(scrollToItem, 30, item)
+    } else {
+        window.scrollTo(0, item.offsetTop)
+    }
+}
+
+var navi_click = function()
+{
+	n_top = document.getElementById('navi_top');
+	n_2016 = document.getElementById('navi_2016');
+	n_2015 = document.getElementById('navi_2015');
+	n_2014 = document.getElementById('navi_2014');
+	n_2013 = document.getElementById('navi_2013');
+	n_others = document.getElementById('navi_others');
+	
+	n_top.onclick = function()
+	{
+		section = document.getElementById('navi');
+		//scrollTo(0, section.offsetTop);
+		scrollToItem(section);
+	}
+	
+	n_2016.onclick = function()
+	{
+		section = document.getElementById('2016');
+		//scrollTo(0, section.offsetTop + 150);
+		scrollToItem(section);
+	}
+	
+	n_2015.onclick = function()
+	{
+		section = document.getElementById('2015');
+		//scrollTo(0, section.offsetTop + 150);
+		scrollToItem(section);
+	}
+	
+	n_2014.onclick = function()
+	{
+		section = document.getElementById('2014');
+		//scrollTo(0, section.offsetTop + 150);
+		scrollToItem(section);
+	}
+	
+	n_2013.onclick = function()
+	{
+		section = document.getElementById('2013');
+		//scrollTo(0, section.offsetTop + 150);
+		scrollToItem(section);
+	}
+	
+	n_others.onclick = function()
+	{
+		//section = document.getElementById('');
+		//scrollTo(0, section.offsetTop);
+	}
 };
